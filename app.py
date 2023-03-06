def17284f2971957bb9599c5bac675f5d29872c2
amadeus0505 <42494998+amadeus0505@users.noreply.github.com>
2023-03-06 07:42:02 +0100

    implemented first post save structure (Issue #2)

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from functools import wraps
 from flask import Flask, render_template, g, session, request, redirect, url_for
+import json
+import os
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
@@ -11,7 +13,30 @@ def check_login(username, password):
     return False
 
 
+def get_posts(category):
+    all_posts = []
+    file_names = os.listdir(f".\\data\\{category}")
+    for file_name in file_names:
+        with open(f".\\data\\{category}\\{file_name}", "r") as file:
+            all_posts.append(json.loads(file.read()))
+    return all_posts
+
+
+def get_post(category, id):
+    if category not in os.listdir(".\\data"):
+        return None
+    if f"{id}.json" not in os.listdir(f".\\data\\{category}"):
+        return None
+
+    with open(f".\\data\\{category}\\{id}.json", "r") as file:
+        post = json.loads(file.read())
+    return post
+
+
+
 def login_required(f):
+    """decorator function if a site should only be visible, if the user is logged in"""
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if "username" not in session.keys():
@@ -22,7 +47,7 @@ def login_required(f):
 
 
 @app.route('/')
-def home():  # put application's code here
+def home():
     return render_template("index.j2")
 
 
@@ -32,38 +57,8 @@ def login():
         if check_login(request.form["username"], request.form["password"]):
             session["username"] = request.form["username"]
             return redirect("/")
-        return redirect("/login?error=true")
-    if "error" in request.args.keys():
-        g.error = request.args["error"]
+        g.error = True
     return render_template("login.j2")
-
-
-@app.route("/logout")
-def logout():
-    del session["username"]
-
-
-@app.route("/category/<cat>")
-def category(cat):
-    g.cat = cat
-    session["posts"] = [
-        {"title": "TITEL des 1. Posts", "desc": "Projektbeschreibung 1", "id": 0},
-        {"title": "TITEL des 2. Posts", "desc": "Projektbeschreibung 2", "id": 1},
-        {"title": "TITEL des 3. Posts", "desc": "Projektbeschreibung 3", "id": 2},
-        {"title": "TITEL des 4. Posts", "desc": "Projektbeschreibung 4", "id": 3}
-    ]
-    return render_template("categories.j2")
-
-
-@app.route("/post/<post_id>")
-def post(post_id):
-    try:
-        post = [post for post in session["posts"] if post["id"] == int(post_id)][0]
-    except IndexError:
-        return "", 404
-    g.titel = post["title"]
-    g.desc = post["desc"]
-    return str(post)
 
 
 @app.route("/logout")
@@ -73,5 +68,23 @@ def logout():
     return redirect("/")
 
 
+@app.route("/category/<cat>")
+def category(cat):
+    g.cat = cat
+    g.posts = get_posts(cat)
+    return render_template("categories.j2")
+
+
+@app.route("/post/<category>/<post_id>")
+def post(category, post_id):
+    post = get_post(category, post_id)
+    if post is not None:
+        g.titel = post["title"]
+        g.desc = post["desc"]
+        return str(post)
+    else:
+        return "", 404
+
+
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=80, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
